8406d7839078e5aa94d1f54b6e2495837eed4272
Silvio Limeira <sicemal@gmail.com>
2022-05-17 10:11:41 -0300

    Initial clean version

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1 className="container">Initial clean version</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
